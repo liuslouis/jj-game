43744b4f252620b47cb327b79814204ff12f357a
Peng <pw2393@columbia.edu>
2017-11-04 20:13:10 -0400

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -1,17 +1,22 @@
 class User:
-    def __init__(self, user_name, auth_playerids):
-        self.user_name = user_name
-        self.auth_playerids = auth_playerids
-        self.oppo_playerids = list(set(range(6)) - set(self.auth_playerids))
-class App:
+    def __init__(self):
+        self.id = id # int
+        self.auth_playerids = []#auth_playerids
+        self.oppo_playerids = []#list(set(range(6)) - set(self.auth_playerids))
 
-    def table_login(self, user_name):
-        if len(self.current_users) == 0: 
-            self.current_users.append(User(user_name, [0,2,4]))
-        elif len(self.current_users) == 1: 
-            self.current_users.append(User(user_name, [1,3,5]))
-        else:
-            self.current_users.append(User(user_name, []))
+user1 = User()
+# global user list: app.current_users 
+
+
+def table_login(user):
+    if len(self.current_users) == 0: 
+        app.current_users.append(User(user_name, [0,2,4]))
+    elif len(self.current_users) == 1: 
+        app.current_users.append(User(user_name, [1,3,5]))
+    else:
+        app.current_users.append(User(user_name, []))
 
         
+table_login(user1)
+   
 
